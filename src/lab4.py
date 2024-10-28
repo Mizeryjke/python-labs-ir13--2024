6c0b8587293ea7a7051968fe6ded8c7689332cb6
Mizeryjke <romkokotsyr@gmail.com>
2024-10-28 20:20:57 +0200

    added lab4,lab's task was to create clasll about lamps

--- src/lab4.py
+++ src/lab4.py
@@ -1,12 +1,11 @@
 class Lamp:
-    # Конструктор за замовчуванням
-    def __init__(self, type='LED', power=0, led_count=0, manufacturer='Unknown'):
-        self.__type = type               
-        self.__power = power               
-        self.__led_count = led_count      
-        self.__manufacturer = manufacturer 
-        self.warranty_period = 1         
-        self.color = "White"            
+    def __init__(self, type="LED", power=0, led_count=0, manufacturer="Unknown"):
+        self.__type = type
+        self.__power = power
+        self.__led_count = led_count
+        self.__manufacturer = manufacturer
+        self.warranty_period = 1
+        self.color = "White"
 
     def get_type(self):
         return self.__type
@@ -21,17 +20,23 @@ class Lamp:
         return self.__manufacturer
 
     def __str__(self):
-        return (f"Lamp(type={self.__type}, power={self.__power}W, led_count={self.__led_count}, "
-                f"manufacturer={self.__manufacturer}, warranty_period={self.warranty_period}, color={self.color})")
+        return (
+            f"Lamp(type={self.__type}, power={self.__power}W, led_count={self.__led_count}, "
+            f"manufacturer={self.__manufacturer}, warranty_period={self.warranty_period}, color={self.color})"
+        )
 
     def __repr__(self):
-        return (f"Lamp(type='{self.__type}', power={self.__power}, led_count={self.__led_count}, "
-                f"manufacturer='{self.__manufacturer}', warranty_period={self.warranty_period}, color='{self.color}')")
+        return (
+            f"Lamp(type='{self.__type}', power={self.__power}, led_count={self.__led_count}, "
+            f"manufacturer='{self.__manufacturer}', warranty_period={self.warranty_period}, color='{self.color}')"
+        )
 
     def __del__(self):
-        print(f"Lamp object with type '{self.__type}' and manufacturer '{self.__manufacturer}' is being deleted")
+        print(
+            f"Lamp object with type '{self.__type}' and manufacturer '{self.__manufacturer}' is being deleted"
+        )
 
-# Основний метод для ініціалізації об'єктів
+
 def main():
     lamp1 = Lamp("LED", 60, 10, "Philips")
     lamp2 = Lamp("Halogen", 100, 5, "Osram")
@@ -40,6 +45,7 @@ def main():
     print(lamp1)
     print(lamp2)
     print(lamp3)
+
 
 if __name__ == "__main__":
     main()
